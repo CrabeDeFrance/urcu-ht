23f6ddb1bfd0245bd43eeeb8ac3563c6c2489dbe
CrabeDeFrance <crabe.france@gmail.com>
2022-01-06 23:52:20 +0100

    rework api and objects protection

--- src/lib.rs
+++ src/lib.rs
@@ -25,19 +25,23 @@
 //!     let ht = ht.clone();
 //!     std::thread::spawn(move || {
 //!         // Get a read handle for this thread
-//!         let read = ht.read();
+//!         let ht = ht.thread();
 //!         // wait until main thread adds some reviews
 //!         std::thread::sleep(std::time::Duration::from_millis(100));
 //!         
+//!         let read = ht.rdlock();
 //!         let review = read.get_clone(&"Adventures of Huckleberry Finn".to_string());
 //!         match review {
 //!             Some(review) => println!("{}: {}", "Adventures of Huckleberry Finn", review),
-//!             None => println!("{} is unreviewed.", book)
+//!             None => println!("{} is unreviewed.", "Adventures of Huckleberry Finn")
 //!         }
+//!
+//!         // read lock is released when leaving this block
 //!     })
 //! };
 //!
-//! let mut write = ht.write().mutex().lock().unwrap();
+//! let ht = ht.thread();
+//! let mut write = ht.wrlock().unwrap();
 //! write.insert_or_replace("Adventures of Huckleberry Finn".to_string(),
 //!     "My favorite book.".to_string());
 //! write.insert_or_replace("Grimms' Fairy Tales".to_string(),
@@ -48,8 +52,8 @@
 use std::cell::Cell;
 use std::hash::{Hash, Hasher};
 use std::marker::PhantomData;
-use std::sync::Mutex;
 use std::sync::Once;
+use std::sync::{Mutex, MutexGuard};
 
 /// Possible error types returned by this module
 #[derive(Debug)]
@@ -80,7 +84,7 @@ impl Rcu {
 /// An RcuHt object is an instance of a RCU hashtable.
 pub struct RcuHt<K, V> {
     /// mutex to protect writer (write operation must be done under lock)
-    mutex: Mutex<RcuHtWriterProtected<K, V>>,
+    mutex: Mutex<RcuHtWriterGuard<K, V>>,
     /// a pointer to an instance of lib urcu hashtable
     urcuht: *mut urcu_sys::cds_lfht,
 }
@@ -92,7 +96,7 @@ unsafe impl<K, V> Sync for RcuHt<K, V> {}
 
 impl<K, V> RcuHt<K, V>
 where
-    K: Hash + Eq + Clone,
+    K: Hash + Eq,
     V: Clone,
 {
     /// Allocate a new instance of urcu hashtable.
@@ -133,20 +137,15 @@ where
                 return Err(RcuError::InvalidParameters);
             }
 
-            let mutex = Mutex::new(RcuHtWriterProtected::new(urcuht));
+            let mutex = Mutex::new(RcuHtWriterGuard::new());
 
             Ok(RcuHt { urcuht, mutex })
         }
     }
 
-    /// Get a read handle. Can be used to read data from hashtable.
-    pub fn read(&self) -> RcuHtReader<K, V> {
-        RcuHtReader::new(self.urcuht)
-    }
-
-    /// Get a write handle. Can be used to read or write data.
-    pub fn write(&self) -> RcuHtWriter<K, V> {
-        RcuHtWriter::new(&self.mutex)
+    /// Get a per thread handle. Will be used for read/write operations.
+    pub fn thread(&self) -> RcuHtThread<K, V> {
+        RcuHtThread::new(self.urcuht, &self.mutex)
     }
 }
 
@@ -220,7 +219,6 @@ unsafe fn urcu_cds_lfht_head_to_rust_type<K, V>(
 unsafe fn urcu_get_node<K, V>(ht: *mut urcu_sys::cds_lfht, key: &K) -> *mut urcu_sys::cds_lfht_node
 where
     K: Hash + Eq,
-    V: Clone,
 {
     let hash = urcu_key_hash(key);
 
@@ -260,7 +258,6 @@ fn urcu_key_hash<K: Hash>(data: &K) -> u64 {
 unsafe extern "C" fn urcu_free_node<K, V>(head: *mut urcu_sys::rcu_head)
 where
     K: Hash + Eq,
-    V: Clone,
 {
     let node = urcu_cds_lfht_head_to_rust_type::<K, V>(head);
 
@@ -277,17 +274,16 @@ thread_local! {
     static URCU_THREAD_REGISTERED_COUNT: Cell<u32>  = Cell::new(0);
 }
 
-/// Per thread object used to provide safe read access to RCU hashtable.
+/// Per thread object used to provide safe access to RCU hashtable.
 ///
 /// It registers the current thread if needed (the first reader or writer object triggers the registration).
 /// It unregisters the current thread when no more objects are alive in this thread.
-pub struct RcuHtReader<K, V> {
-    phantom_key: PhantomData<K>,
-    phantom_val: PhantomData<V>,
+pub struct RcuHtThread<'ht, K, V> {
     urcuht: *mut urcu_sys::cds_lfht,
+    mutex: &'ht Mutex<RcuHtWriterGuard<K, V>>,
 }
 
-impl<K, V> RcuHtReader<K, V>
+impl<'ht, K, V> RcuHtThread<'ht, K, V>
 where
     K: Hash + Eq,
     V: Clone,
@@ -296,7 +292,7 @@ where
     /// A different handle is needed for each thread doing "read" operations.
     /// It registers this thread in urcu lib.
     /// It must stick to a single thread. One must not try to move this handle between threads.
-    pub fn new(urcuht: *mut urcu_sys::cds_lfht) -> Self {
+    pub fn new(urcuht: *mut urcu_sys::cds_lfht, mutex: &'ht Mutex<RcuHtWriterGuard<K, V>>) -> Self {
         // manage thread reference counter : if the count is 1 => register this thread
         let thread_count = URCU_THREAD_REGISTERED_COUNT.with(|cell| {
             let mut thread_count = cell.get();
@@ -312,11 +308,72 @@ where
         }
 
         // return an object with a pointer to the hashtable
-        // TODO add a reference to Rust RcuHt to be sure it cannot be deleted before main hashtable ?
-        RcuHtReader {
+        // Return an object with a reference to a shared write mutex for this hashtable.
+        RcuHtThread {
+            urcuht,
+            // This prevent concurrent write on this hashtable.
+            // Since mutex is a reference, we are sure original hashtable cannot be deleted before this object.
+            // This is needed to protect hashtable deletion.
+            mutex,
+        }
+    }
+
+    pub fn wrlock(&self) -> Option<RcuHtWriter<K, V>> {
+        match self.mutex.lock() {
+            Ok(guard) => Some(RcuHtWriter::new(self.urcuht, self, guard)),
+            Err(_err) => None,
+        }
+    }
+
+    pub fn rdlock(&self) -> RcuHtRead<K, V> {
+        RcuHtRead::new(self.urcuht, self)
+    }
+}
+
+impl<'ht, K, V> Drop for RcuHtThread<'ht, K, V> {
+    fn drop(&mut self) {
+        /* manage thread reference counter : if the count is 0 (last object) => unregister this thread */
+        let thread_count = URCU_THREAD_REGISTERED_COUNT.with(|cell| {
+            let mut thread_count = cell.get();
+            thread_count -= 1;
+            cell.set(thread_count);
+            thread_count
+        });
+
+        if thread_count == 0 {
+            unsafe {
+                urcu_sys::rcu_unregister_thread();
+            }
+        }
+    }
+}
+
+pub struct RcuHtRead<'thread, 'ht, K, V> {
+    phantom_key: PhantomData<K>,
+    phantom_val: PhantomData<V>,
+    urcuht: *mut urcu_sys::cds_lfht,
+    _thread: &'thread RcuHtThread<'ht, K, V>,
+}
+
+impl<'thread, 'ht, K, V> RcuHtRead<'thread, 'ht, K, V>
+where
+    K: Hash + Eq,
+    V: Clone,
+{
+    /// Get a new "read" handle.
+    /// A different handle is needed for each thread doing "read" operations.
+    /// It registers this thread in urcu lib.
+    /// It must stick to a single thread. One must not try to move this handle between threads.
+    pub fn new(urcuht: *mut urcu_sys::cds_lfht, thread: &'thread RcuHtThread<'ht, K, V>) -> Self {
+        unsafe {
+            urcu_sys::rcu_read_lock();
+        }
+
+        RcuHtRead {
             phantom_key: PhantomData,
             phantom_val: PhantomData,
             urcuht,
+            _thread: thread,
         }
     }
 
@@ -324,94 +381,37 @@ where
         let mut ret: Option<V> = None;
 
         unsafe {
-            urcu_sys::rcu_read_lock();
-
             let found_node = urcu_get_node::<K, V>(self.urcuht, key);
 
             if !found_node.is_null() {
                 let node = urcu_cds_lfht_node_to_rust_type::<K, V>(found_node);
                 ret = Some((*node).data.clone());
             }
-
-            urcu_sys::rcu_read_unlock();
         }
 
         ret
     }
 }
 
-impl<K, V> Drop for RcuHtReader<K, V> {
+impl<'thread, 'ht, K, V> Drop for RcuHtRead<'thread, 'ht, K, V> {
     fn drop(&mut self) {
         /* manage thread reference counter : if the count is 0 (last object) => unregister this thread */
-        let thread_count = URCU_THREAD_REGISTERED_COUNT.with(|cell| {
-            let mut thread_count = cell.get();
-            thread_count -= 1;
-            cell.set(thread_count);
-            thread_count
-        });
-
-        if thread_count == 0 {
-            unsafe {
-                urcu_sys::rcu_unregister_thread();
-            }
+        unsafe {
+            urcu_sys::rcu_read_unlock();
         }
     }
 }
 
-/// Per thread object used to provide safe write access to RCU hashtable.
-///
-/// It registers the current thread if needed (the first reader or writer object triggers the registration).
-/// It unregisters the current thread when no more objects are alive in this thread.
-pub struct RcuHtWriter<'a, K, V> {
-    mutex: &'a Mutex<RcuHtWriterProtected<K, V>>,
+pub struct RcuHtWriterGuard<K, V> {
+    phantom_key: PhantomData<K>,
+    phantom_val: PhantomData<V>,
 }
 
-impl<'a, K, V> RcuHtWriter<'a, K, V>
-where
-    K: Hash,
-{
-    fn new(mutex: &Mutex<RcuHtWriterProtected<K, V>>) -> RcuHtWriter<K, V> {
-        // manage thread reference counter : if the count is 1 => register this thread
-        // Indeed, threads calling add or del API need to be done in registered RCU read-side threads.
-        let thread_count = URCU_THREAD_REGISTERED_COUNT.with(|cell| {
-            let mut thread_count = cell.get();
-            thread_count += 1;
-            cell.set(thread_count);
-            thread_count
-        });
-
-        if thread_count == 1 {
-            unsafe {
-                urcu_sys::rcu_register_thread();
-            }
-        }
-
-        // Return an object with a reference to a shared write mutex for this hashtable.
-        // This prevent concurrent write on this hashtable.
-        // Since mutex is a reference, we are sure original hashtable cannot be deleted before this object.
-        // This is needed to protect hashtable deletion.
-        RcuHtWriter { mutex }
-    }
-
-    pub fn mutex(&self) -> &'a Mutex<RcuHtWriterProtected<K, V>> {
-        self.mutex
-    }
-}
-
-impl<'a, K, V> Drop for RcuHtWriter<'a, K, V> {
-    fn drop(&mut self) {
-        /* manage thread reference counter : if the count is 0 (last object) => unregister this thread */
-        let thread_count = URCU_THREAD_REGISTERED_COUNT.with(|cell| {
-            let mut thread_count = cell.get();
-            thread_count -= 1;
-            cell.set(thread_count);
-            thread_count
-        });
-
-        if thread_count == 0 {
-            unsafe {
-                urcu_sys::rcu_unregister_thread();
-            }
+impl<K, V> RcuHtWriterGuard<K, V> {
+    fn new() -> Self {
+        RcuHtWriterGuard {
+            phantom_key: PhantomData,
+            phantom_val: PhantomData,
         }
     }
 }
@@ -420,26 +420,31 @@ impl<'a, K, V> Drop for RcuHtWriter<'a, K, V> {
 ///
 /// It can only be called under locked mutex to protect from concurrent access.
 /// It must not be shared between threads.
-pub struct RcuHtWriterProtected<K, V> {
-    phantom_key: PhantomData<K>,
-    phantom_val: PhantomData<V>,
+pub struct RcuHtWriter<'guard, 'thread, 'ht, K, V> {
     urcuht: *mut urcu_sys::cds_lfht,
+    // keep references to thread so object cannot be destroyed in an invalid order
+    _thread: &'thread RcuHtThread<'ht, K, V>,
+    // have the guard here so lock will be released when writer is destroyed
+    _guard: MutexGuard<'guard, RcuHtWriterGuard<K, V>>,
 }
 
-impl<'a, K, V> RcuHtWriterProtected<K, V>
+impl<'guard, 'thread, 'ht, K, V> RcuHtWriter<'guard, 'thread, 'ht, K, V>
 where
-    K: Hash + Eq + Clone,
-    V: Clone,
+    K: Hash + Eq,
 {
     /// Creates a write instance.
     ///
     /// There should be only one single instance allocated under the write mutex.
-    fn new(urcuht: *mut urcu_sys::cds_lfht) -> RcuHtWriterProtected<K, V> {
+    fn new(
+        urcuht: *mut urcu_sys::cds_lfht,
+        thread: &'thread RcuHtThread<'ht, K, V>,
+        guard: MutexGuard<'guard, RcuHtWriterGuard<K, V>>,
+    ) -> RcuHtWriter<'guard, 'thread, 'ht, K, V> {
         // return an object containing the pointer to the hashtable
-        RcuHtWriterProtected {
-            phantom_key: PhantomData,
-            phantom_val: PhantomData,
+        RcuHtWriter {
             urcuht,
+            _thread: thread,
+            _guard: guard,
         }
     }
 
@@ -561,27 +566,73 @@ mod tests {
         let child = {
             let ht = ht.clone();
             std::thread::spawn(move || {
-                // Get a read handle for this thread
-                let read = ht.read();
-                // wait until main thread adds some reviews
-                std::thread::sleep(std::time::Duration::from_millis(100));
+                {
+                    // Get a read handle for this thread
+                    let ht = ht.thread();
+                    // wait until main thread adds some reviews
+                    std::thread::sleep(std::time::Duration::from_millis(100));
 
-                let review = read.get_clone(&"Adventures of Huckleberry Finn".to_string());
-                assert_eq!(review.is_some(), true);
-                assert_eq!(review.unwrap(), "My favorite book.".to_string());
+                    let rdlock = ht.rdlock();
+
+                    let review = rdlock.get_clone(&"Adventures of Huckleberry Finn".to_string());
+                    assert_eq!(review.is_some(), true);
+                    assert_eq!(review.unwrap(), "My favorite book.".to_string());
+                }
+
+                // uncomment this to check rdlock cannot live longer than ht thread
+                /*
+                let _out_error = {
+                    // Get a read handle for this thread
+                    let ht = ht.thread();
+
+                    // get a rdlock and return it
+                    ht.rdlock()
+                };
+                */
+
+                // uncommend this to check wrlock cannot live longer than ht thread
+                /*
+                let _out_error = {
+                    // Get a read handle for this thread
+                    let ht = ht.thread();
+
+                    // get a rdlock and return it
+                    ht.wrlock()
+                };
+                */
             })
         };
 
-        let mut write = ht.write().mutex().lock().unwrap();
-        write.insert_or_replace(
-            "Adventures of Huckleberry Finn".to_string(),
-            "My favorite book.".to_string(),
-        );
-        write.insert_or_replace(
-            "Grimms' Fairy Tales".to_string(),
-            "Masterpiece.".to_string(),
-        );
+        let ht = ht.thread();
+        {
+            let mut wrlock = ht.wrlock().unwrap();
+            wrlock.insert_or_replace(
+                "Adventures of Huckleberry Finn".to_string(),
+                "My favorite book.".to_string(),
+            );
+        }
+
+        {
+            let mut wrlock = ht.wrlock().unwrap();
+            wrlock.insert_or_replace(
+                "Grimms' Fairy Tales".to_string(),
+                "Masterpiece.".to_string(),
+            );
+        }
 
         child.join().expect("cannot join thread");
+
+        // uncommend this to check RcuHtThread cannot live longer than RcuHt
+        /*
+        let _out_error = {
+            // allocate a new hashtable
+            let ht = RcuHt::<String,String>::new(64, 64, 64, false).unwrap();
+            // Get a read handle for this thread
+            let thread = ht.thread();
+
+            // get a rdlock and return it
+            thread
+        };
+        */
     }
 }
